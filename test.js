468745817681bae13e6429490863c5be30a4e324
Jeremy Stanley <jeremy@obvious.com>
2012-06-28 18:27:11 -0700

    Updated asyncBuilder to support promises and wrapped arguments

--- test.js
+++ test.js
@@ -1,60 +1,268 @@
 var asyncBuilder = require("./asyncBuilder")
+var q = require("q")
 
-  , getUser = function(req, next) {
-      next(null, req.user)
+var testMethods = [
+    testSynchronous
+  , testGetters
+  , testCallbacks
+  , testPromises
+  , testPromisesAndGetters
+  , testThrowCallback
+  , testThrowSynchronouslyCallback
+  , testThrowPromises
+  , testThrowSynchronouslyPromises
+]
+function runNextTest() {
+  if (testMethods.length) {
+    testMethod = testMethods.shift()
+    testMethod(runNextTest)
+  }
+}
+runNextTest()
+
+/**
+ * Test the builder with methods that return synchronously (usePromises() supports synchronous returns)
+ */
+function testSynchronous(next) {
+  console.log("testing synchronous")
+  var factory = new asyncBuilder.BuilderFactory()
+  factory.usePromises()
+  factory.add("currentUser", getCurrentUser, ["req"])
+  factory.add("millisNow", getMillisNow)
+  factory.add("secondsNow", getSecondsNow, ["millisNow"])
+  factory.add("userSalt", createUserSalt, ["currentUser.username", "secondsNow"])
+  testFactory(factory, next)
+}
+
+/**
+ * Test the builder with methods that return synchronously and expect arguments in the form of objects
+ * with a get() method
+ */
+function testGetters(next) {
+  console.log("testing getters")
+  var factory = new asyncBuilder.BuilderFactory()
+  factory.usePromises()
+  factory.wrapInputs()
+  factory.add("currentUser", withGetters(getCurrentUser), ["req"])
+  factory.add("millisNow", withGetters(getMillisNow))
+  factory.add("secondsNow", withGetters(getSecondsNow), ["millisNow"])
+  factory.add("userSalt", withGetters(createUserSalt), ["currentUser.username", "secondsNow"])
+  testFactory(factory, next)
+}
+
+/**
+ * Test the builder with methods that expect to return via a node-style callback (function(err, response))
+ * as the last argument
+ */
+function testCallbacks(next) {
+  console.log("testing callbacks")
+  var factory = new asyncBuilder.BuilderFactory()
+  factory.add("currentUser", withCallback(getCurrentUser), ["req"])
+  factory.add("millisNow", withCallback(getMillisNow))
+  factory.add("secondsNow", withCallback(getSecondsNow), ["millisNow"])
+  factory.add("userSalt", withCallback(createUserSalt), ["currentUser.username", "secondsNow"])
+  testFactory(factory, next)
+}
+
+/**
+ * Test the builder with methods that expect to return via promises
+ */
+function testPromises(next) {
+  console.log("testing promises")
+  var factory = new asyncBuilder.BuilderFactory()
+  factory.usePromises()
+  factory.add("currentUser", withPromise(getCurrentUser), ["req"])
+  factory.add("millisNow", withPromise(getMillisNow))
+  factory.add("secondsNow", withPromise(getSecondsNow), ["millisNow"])
+  factory.add("userSalt", withPromise(createUserSalt), ["currentUser.username", "secondsNow"])
+  testFactory(factory, next)
+}
+
+/**
+ * Test the builder with promises & and getters
+ */
+function testPromisesAndGetters(next) {
+  console.log("testing promises and getters")
+  var factory = new asyncBuilder.BuilderFactory()
+  factory.usePromises()
+  factory.wrapInputs()
+  factory.add("currentUser", withPromise(withGetters(getCurrentUser)), ["req"])
+  factory.add("millisNow", withPromise(withGetters(getMillisNow)))
+  factory.add("secondsNow", withPromise(withGetters(getSecondsNow)), ["millisNow"])
+  factory.add("userSalt", withPromise(withGetters(createUserSalt)), ["currentUser.username", "secondsNow"])
+  testFactory(factory, next)
+}
+
+/**
+ * Test throwing errors through a callback
+ */
+function testThrowCallback(next) {
+  console.log("testing throwing through callbacks")
+  var factory = new asyncBuilder.BuilderFactory()
+  factory.add("currentUser", withCallback(getCurrentUser), ["req"])
+  factory.add("millisNow", withCallback(getMillisNow))
+  factory.add("secondsNow", withCallback(getSecondsNow), ["millisNow"])
+  factory.add("userSalt", throwCallback(createUserSalt), ["currentUser.username", "secondsNow"])
+  testFactory(factory, next)
+}
+
+/**
+ * Test throwing errors synchronously
+ */
+function testThrowSynchronouslyCallback(next) {
+  console.log("testing throwing synchronously through callbacks")
+  var factory = new asyncBuilder.BuilderFactory()
+  factory.add("currentUser", withCallback(getCurrentUser), ["req"])
+  factory.add("millisNow", withCallback(getMillisNow))
+  factory.add("secondsNow", withCallback(getSecondsNow), ["millisNow"])
+  factory.add("userSalt", throwSynchronously(createUserSalt), ["currentUser.username", "secondsNow"])
+  testFactory(factory, next)
+}
+
+/**
+ * Test throwing errors through a promise
+ */
+ function testThrowPromises(next) {
+  console.log("testing throwing through promises")
+  var factory = new asyncBuilder.BuilderFactory()
+  factory.usePromises()
+  factory.wrapInputs()
+  factory.add("currentUser", withPromise(withGetters(getCurrentUser)), ["req"])
+  factory.add("millisNow", withPromise(withGetters(getMillisNow)))
+  factory.add("secondsNow", withPromise(withGetters(getSecondsNow)), ["millisNow"])
+  factory.add("userSalt", throwPromise(withGetters(createUserSalt)), ["currentUser.username", "secondsNow"])
+  testFactory(factory, next)
+}
+
+/**
+ * Test throwing errors through a promise
+ */
+ function testThrowSynchronouslyPromises(next) {
+  console.log("testing throwing synchronously through promises")
+  var factory = new asyncBuilder.BuilderFactory()
+  factory.usePromises()
+  factory.wrapInputs()
+  factory.add("currentUser", withPromise(withGetters(getCurrentUser)), ["req"])
+  factory.add("millisNow", withPromise(withGetters(getMillisNow)))
+  factory.add("secondsNow", withPromise(withGetters(getSecondsNow)), ["millisNow"])
+  factory.add("userSalt", throwSynchronously(withGetters(createUserSalt)), ["currentUser.username", "secondsNow"])
+  testFactory(factory, next)
+}
+
+/**
+ * Take a specified factory and create a builder to create userSalt
+ */
+function testFactory(factory, next) {
+  // basic request object for testing
+  var request = {
+    currentUser: {
+      username: "jeremy123"
     }
+  }
 
-  , getUserName = function(user, next) {
-      next(null, user.name)
-   }
+  // create and run the builder
+  var builder = factory.newBuilder(["userSalt"])
+  builder.build({req: request}, function (err, data) {
+    console.log("DONE", err, data)
+    if(next) next()
+  })
+}
 
-  , getFirstName = function(name, next) {
-      next(null, name.first)
-    }
-
-  , getLastName = function(name, next) {
-      next(null, name.last)
-    }
-
-  , getAge = function(user, next) {
-      next(null, user.age)
-    }
-
-  , getFirstNamePlusTimestamp = function(firstName, timestamp, next) {
-      next(null, firstName + ":" + timestamp)
-    }
-
-  , getCurrentDate = function(next) {
-      next(null, Date.now())
-    }
-
-  , getCurrentTimestamp = function(date, next) {
-      next(null, Math.floor(date/1000))
-    }
-
-var TestBuilderFactory = new asyncBuilder.BuilderFactory()
-  .add("name", getUserName, ["req.user"])
-  .add("firstName", getFirstName, ["name"])
-  .add("lastName", getLastName, ["name"])
-  .add("age", getAge, ["req.user"])
-
-var SecondTestBuilderFactory = TestBuilderFactory.clone()
-  .add("currentDate", getCurrentDate)
-  .add("currentTimestamp", getCurrentTimestamp, ["currentDate"])
-  .add("namePlusTimestamp", getFirstNamePlusTimestamp, ["firstName", "currentTimestamp"])
-
-var req = {
-  user: {
-    name: {
-      first: "Jeremy"
-    , last: "Stanley"
-    }
-  , age: 28
+/**
+ * Wrap a function such that all of it's arguments' get() methods are called
+ */
+function withGetters(fn) {
+  return function () {
+    var args = Array.prototype.map.call(arguments, function (arg) {
+      return arg.get()
+    })
+    return fn.apply(null, args)
   }
 }
 
-var builder = SecondTestBuilderFactory.newBuilder(["firstName", "lastName", "age", "namePlusTimestamp"])
-  .trace()
-  .build({req:req}, function(err, data) {
-    console.log("-------DONE-------", arguments)
-  })
+/**
+ * Wrap a function in a callback handler
+ */
+function withCallback(fn) {
+  return function () {
+    var args = Array.prototype.slice.call(arguments, 0)
+    var next = args.pop()
+    next(null, fn.apply(null, args))
+  }
+}
+
+/**
+ * Wrap a function in a callback that expects a promise
+ */
+function withPromise(fn) {
+  return function () {
+    var d = q.defer()
+    var args = Array.prototype.slice.call(arguments, 0)
+    process.nextTick(function () {
+      d.resolve(fn.apply(null, args))
+    })
+    return d.promise
+  }
+}
+
+/**
+ * Throw an error synchronously
+ */
+function throwSynchronously(fn) {
+  return function() {
+    throw new Error("I'm done here")
+  }
+}
+
+/**
+ * Throw an error through a callback
+ */
+ function throwCallback(fn) {
+  return function() {
+    var args = Array.prototype.slice.call(arguments, 0)
+    var next = args.pop()
+    next(new Error("I'm done here"))
+  }
+}
+
+/**
+ * Throw an error through a promise
+ */
+ function throwPromise(fn) {
+  return function () {
+    var d = q.defer()
+    var args = Array.prototype.slice.call(arguments, 0)
+    process.nextTick(function () {
+      d.reject(new Error("I'm done here"))
+    })
+    return d.promise
+  }
+ }
+
+/**
+ * Retrieve the current user from the request object synchronously
+ */
+function getCurrentUser(req) {
+  return req.currentUser
+}
+
+/**
+ * Get the current time in milliseconds synchronously
+ */
+function getMillisNow() {
+  return Date.now()
+}
+
+/**
+ * Get the current time in seconds and return synchronously
+ */
+function getSecondsNow(millis) {
+  return Math.floor(millis/1000)
+}
+
+/**
+ * Create a user salt and return synchronously
+ */
+function createUserSalt(username, millis) {
+  return username + ":" + millis
+}
