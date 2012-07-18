4eb94696a31b6e527e2e8f2960008b336c4701c8
Jeremy Stanley <jeremy@obvious.com>
2012-07-17 18:51:36 -0700

    Incrementing trace iterations

--- lib2/BuilderInstance.js
+++ lib2/BuilderInstance.js
@@ -188,7 +188,7 @@ BuilderInstance.prototype._resolve = function (data, nodeName) {
       this._trace(data, {node: nodeName, action: 'resolved'})
     }.bind(this))
     promise.fail(function (err) {
-      if (traceIterations >= 30 && traceInterval) clearInterval(traceInterval)
+      if (traceIterations++ >= 30 && traceInterval) clearInterval(traceInterval)
       this._trace(data, {node: nodeName, action: 'failed'})
     }.bind(this))
   }
