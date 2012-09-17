0c93b133ae37849ca2624589595ab41f830cb0d7
Jeremy Stanley <jeremy@obvious.com>
2012-09-17 11:23:04 -0700

    Added comments for DOT graph creation

--- test/dot.js
+++ test/dot.js
@@ -11,25 +11,22 @@ tester.setUp = function (done) {
   done()
 }
 
-// test passing args inline vs chained
+// test creating a dot graph from a given
 tester.testDotGraph = function (test) {
   this.graph.add('name-fromLiteral', {_literal: 'Jeremy'})
   this.graph.add('name-toUpper', function (name) {return name.toUpperCase()}, ['name'])
+  this.graph.add('name-toLower', function (name) {return name.toLowerCase()}, ['name'])
   this.graph.add('age', 5)
 
   var dotOutput = this.graph.newAsyncBuilder()
     .builds('name-toUpper').using('name-fromLiteral')
+    .builds('name-toLower').using('req.query.name')
     .builds('age')
     .getDotGraph(['req', 'res'])
 
-  test.equal(dotOutput, 'digraph G {\n' +
-  '  "BUILDER_OUTPUT" [ color = "green" ];\n' +
-  '  "name-toUpper (name-toUpper)" [ color = "orange" ];\n' +
-  '  "name-fromLiteral (name-fromLiteral)" [ color = "blue" ];\n' +
-  '  "age (age)" [ color = "orange" ];\n' +
-  '  "name-fromLiteral (name-fromLiteral)" -> "name-toUpper (name-toUpper)";\n' +
-  '  "name-toUpper (name-toUpper)" -> "BUILDER_OUTPUT";\n' +
-  '  "age (age)" -> "BUILDER_OUTPUT";\n}\n', "Graph dot output should be predictable")
+  var expectedOutput = 'digraph G {\n  "BUILDER_OUTPUT" [ color = "green" ];\n  "name-toUpper (name-toUpper)" [ color = "orange" ];\n  "name-fromLiteral (name-fromLiteral)" [ color = "blue" ];\n  "name-toLower (name-toLower)" [ color = "orange" ];\n  "req" [ color = "red" ];\n  "req.query" [ color = "red" ];\n  "req.query.name" [ color = "red" ];\n  "age (age)" [ color = "orange" ];\n  "name-fromLiteral (name-fromLiteral)" -> "name-toUpper (name-toUpper)";\n  "name-toUpper (name-toUpper)" -> "BUILDER_OUTPUT";\n  "req" -> "req.query";\n  "req.query" -> "req.query.name";\n  "req.query.name" -> "name-toLower (name-toLower)";\n  "name-toLower (name-toLower)" -> "BUILDER_OUTPUT";\n  "age (age)" -> "BUILDER_OUTPUT";\n}\n'
+
+  test.equal(dotOutput, expectedOutput, "Graph dot output should be predictable")
   test.done()
 }
 
