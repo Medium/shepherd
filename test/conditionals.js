bc3b8fec14b096f51f889cb6ec1ef2d56a8cbc36
Jeremy Stanley <github@azulus.com>
2013-02-18 18:41:02 -0800

    Test of if-like logic in place

--- test/conditionals.js
+++ test/conditionals.js
@@ -7,16 +7,72 @@ exports.setUp = function (done) {
   done()
 }
 
+exports.testIgnoredPrivate = function (test) {
+  function throwsError() {
+    throw new Error("testing")
+  }
+
+  this.graph.add('val-test', throwsError)
+
+  this.graph.newBuilder()
+    .builds('!val-test')
+    .ignoreErrors('val-test')
+    .run()
+    .then(function () {
+      test.ok("Successfully ignored errors")
+    })
+    .fail(function () {
+      test.fail("Failed to ignore errors")
+    })
+    .fin(function () {
+      test.done()
+    })
+}
+
 exports.testEscape = function (test) {
-  this.graph.add('val-gt', this.graph.literal('greater than'))
-  this.graph.add('val-lte', this.graph.literal('less than equal'))
+  var err = new Error("Hello")
 
-  this.graph.add('test-conditional', this.graph.subgraph, ['val1', 'val2'])
-    .if('args.val1').gt('args.val2')
-      .builds('val-gt')
-    .else()
-      .builds('val-lte')
-    .end()
+  function throwsError() {
+    throw err
+  }
 
-  test.done()
+  function shouldBeSkipped() {
+    test.fail("This should be skipped")
+    return false
+  }
+
+  function catchesError(val) {
+    try {
+      val.get()
+      test.fail("An error should be thrown")
+      return false
+    } catch (e) {
+      test.equal(e, err, "Error should be thrown error")
+      return true
+    }
+  }
+
+  this.graph.add('err-thrown', throwsError)
+  this.graph.add('bool-shouldBeSkipped', shouldBeSkipped)
+  this.graph.add('bool-caught', catchesError, ['bool'])
+    .withGetters()
+
+  this.graph.newBuilder()
+    .builds('!err-thrown')
+    .builds('!bool-shouldBeSkipped')
+      .using('!err-thrown')
+    .builds('bool-caught')
+      .using('bool-shouldBeSkipped')
+    .ignoreErrors('err-thrown', 'bool-shouldBeSkipped')
+    .run()
+    .then(function (data) {
+      test.equal(data['bool-caught'], true, "Error was successfully caught")
+      test.ok("Successfully ignored errors in chain")
+    })
+    .fail(function (e) {
+      test.fail("Error wasn't caught successfully")
+    })
+    .fin(function () {
+      test.done()
+    })
 }
