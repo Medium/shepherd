8f787642b0c880e9367a73c4fab44e52310e04c2
Jeremy Stanley <jeremy@obvious.com>
2013-02-22 15:49:53 -0800

    Adding test with a syntax error

--- test/conditionals.js
+++ test/conditionals.js
@@ -170,3 +170,79 @@ exports.testGuardsWithoutElse = function (test) {
       test.done()
     })
 }
+
+// test guards with syntax error
+exports.testGuardsWithSyntaxError = function (test) {
+  var testInputs = [
+    {name: 'Jeremy', method: 'upper', output: 'JEREMY'},
+    {name: 'Elizabeth', method: 'lower', output: 'elizabeth'},
+    {name: 'Dan', method: 'quotes', output: '"Dan"'},
+    {name: 'Sho', method: 'lower', output: 'sho'},
+    {name: 'Jon', method: 'unspecified', output: undefined}
+  ]
+  this.graph.add('bool-isEqual', function (str1, str2) {
+    return str1 == str2
+  }, ['str1', 'str2'])
+
+  this.graph.add('str-toUpper', function (str) {
+    return str.toUpperCase()
+  }, ['str'])
+
+  this.graph.add('str-toLower', function (str) {
+    return strasdfasdf.toLowerCase()
+  }, ['str'])
+
+  this.graph.add('str-quotes', function (str, myChar) {
+    return myChar + str + myChar
+  }, ['str', 'char'])
+
+  this.graph.add('str-echo', function (str) {
+    return str
+  }, ['str'])
+
+  this.graph.add('char-fromLiteral', function (val) {
+    return val
+  }, ['val'])
+
+  this.graph.add('str-transform', this.graph.subgraph, ['str', 'method'])
+    .describe('str-output')
+      .builds('str-toUpper')
+        .using('args.str')
+        .when({isUpper: 'bool-isEqual'})
+          .using({str1: 'args.method'}, {str2: this.graph.literal('upper')})
+
+      .builds('str-toLower')
+        .using('args.str')
+        .when({isLower: 'bool-isEqual'})
+          .using({str1: 'args.method'}, {str2: this.graph.literal('lower')})
+
+      .builds('!char-fromLiteral')
+            .using({val: this.graph.literal('"')})
+      .builds('str-quotes')
+        .using('args.str', 'char-fromLiteral')
+        .when({isQuote: 'bool-isEqual'})
+          .using({str1: 'args.method'}, {str2: this.graph.literal('quotes')})
+    .end()
+
+  this.graph.newBuilder()
+    .builds({str1: 'str-transform'})
+      .using({str: this.graph.literal(testInputs[0].name)}, {method: this.graph.literal(testInputs[0].method)})
+    .builds({str2: 'str-transform'})
+      .using({str: this.graph.literal(testInputs[1].name)}, {method: this.graph.literal(testInputs[1].method)})
+    .builds({str3: 'str-transform'})
+      .using({str: this.graph.literal(testInputs[2].name)}, {method: this.graph.literal(testInputs[2].method)})
+    .builds({str4: 'str-transform'})
+      .using({str: this.graph.literal(testInputs[3].name)}, {method: this.graph.literal(testInputs[3].method)})
+    .builds({str5: 'str-transform'})
+      .using({str: this.graph.literal(testInputs[4].name)}, {method: this.graph.literal(testInputs[4].method)})
+    .run()
+    .then(function (data) {
+      test.fail("Should fail due to a syntax error")
+    })
+    .fail(function (e) {
+      test.ok("Failed due to a syntax error")
+    })
+    .fin(function () {
+      test.done()
+    })
+}
