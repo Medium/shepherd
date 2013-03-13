9d129a1584b8e2305b833d2042ade0a01753880a
Jeremy Stanley <jeremy@obvious.com>
2013-03-13 12:09:55 -0700

    Pre-loading input literals into the graph and skipping a few function calls

--- test/dot.js
+++ test/dot.js
@@ -22,19 +22,19 @@ exports.testDotGraph = function (test) {
 
   var expectedOutput = 'digraph G {\n  "builderOutput-DOT_TEST_1 (BUILDER OUTPUT)" [ color = "green" ];\n' +
     '  "name-toUpper-peerGroup1 (name-toUpper)" [ color = "orange" ];\n' +
-    '  "name-fromLiteral-peerGroup1 (name-fromLiteral)" [ color = "blue" ];\n' +
+    '  "name-fromLiteral-peerGroup1" [ color = "blue" ];\n' +
     '  "name-toLower-peerGroup1 (name-toLower)" [ color = "orange" ];\n' +
     '  "req" [ color = "red" ];\n' +
     '  "req.query" [ color = "red" ];\n' +
     '  "req.query.name" [ color = "red" ];\n' +
-    '  "age-peerGroup1 (age)" [ color = "orange" ];\n' +
-    '  "name-fromLiteral-peerGroup1 (name-fromLiteral)" -> "name-toUpper-peerGroup1 (name-toUpper)";\n' +
+    '  "age-peerGroup1" [ color = "orange" ];\n' +
+    '  "name-fromLiteral-peerGroup1" -> "name-toUpper-peerGroup1 (name-toUpper)";\n' +
     '  "name-toUpper-peerGroup1 (name-toUpper)" -> "builderOutput-DOT_TEST_1 (BUILDER OUTPUT)";\n' +
     '  "req" -> "req.query";\n' +
     '  "req.query" -> "req.query.name";\n' +
     '  "req.query.name" -> "name-toLower-peerGroup1 (name-toLower)";\n' +
     '  "name-toLower-peerGroup1 (name-toLower)" -> "builderOutput-DOT_TEST_1 (BUILDER OUTPUT)";\n' +
-    '  "age-peerGroup1 (age)" -> "builderOutput-DOT_TEST_1 (BUILDER OUTPUT)";\n}\n'
+    '  "age-peerGroup1" -> "builderOutput-DOT_TEST_1 (BUILDER OUTPUT)";\n}\n'
 
   test.equal(dotOutput, expectedOutput, "Graph dot output should be predictable")
   test.done()
