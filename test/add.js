a4e84f0671e88d1b444877e4554a05ca974a1335
Jeremy Stanley <jeremy@obvious.com>
2013-04-16 16:14:10 -0700

    --

--- test/add.js
+++ test/add.js
@@ -54,41 +54,32 @@ exports.testUndefinedLiteral = function (test) {
 
 // test that literals deduplicate correctly
 exports.testLiteralDeduplication = function (test) {
-  try {
-    this.graph.add('name-a', this.graph.literal('Jeremy'))
-    this.graph.add('name-b', this.graph.literal('Jeremy'))
-    var counter = 0
+  this.graph.add('name-a', this.graph.literal('Jeremy'))
+  this.graph.add('name-b', this.graph.literal('Jeremy'))
+  var counter = 0
 
-    this.graph.add('name-echo', function (name) {
-      console.log(444)
-      counter++
-      return name
-    }, ['name'])
+  this.graph.add('name-echo', function (name) {
+    counter++
+    return name
+  }, ['name'])
 
-    console.log(222)
-    this.graph.newBuilder()
-      .builds({nameA: 'name-echo'})
-        .using('name-a')
-      .builds({nameB: 'name-echo'})
-        .using('name-b')
-      .run()
-      .then(function (data) {
-        console.log(111)
-        test.equal(data.nameA, 'Jeremy', "Name should match")
-        test.equal(data.nameB, 'Jeremy', "Name should match")
-        test.equal(counter, 1, "Echo should only have ran once")
-      })
-      .fail(function (e) {
-        console.log(333)
-        test.fail("An error was returned")
-      })
-      .fin(function () {
-        test.done()
-      })
-  } catch (e) {
-    console.error("WHAT")
-    console.error(e.stack)
-  }
+  this.graph.newBuilder()
+    .builds({nameA: 'name-echo'})
+      .using('name-a')
+    .builds({nameB: 'name-echo'})
+      .using('name-b')
+    .run()
+    .then(function (data) {
+      test.equal(data.nameA, 'Jeremy', "Name should match")
+      test.equal(data.nameB, 'Jeremy', "Name should match")
+      test.equal(counter, 1, "Echo should only have ran once")
+    })
+    .fail(function (e) {
+      test.fail("An error was returned")
+    })
+    .fin(function () {
+      test.done()
+    })
 }
 
 // test that adding the same node twice fails
