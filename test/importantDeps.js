9febe73ecf26510b806252d936bc353d975ea6e3
Nick Santos <nick@medium.com>
2013-07-24 01:06:14 -0400

    Implement a prioritization algorithm to fix loads of bugs in silent nodes.

--- test/importantDeps.js
+++ test/importantDeps.js
@@ -373,3 +373,100 @@ builder.add(function testImportantChild(test) {
       test.equal(0, sentEmail)
     })
 })
+
+builder.add(function testImportantDescendant(test) {
+  var log = ''
+  graph.add('add-v', function (v) {
+    log += v
+    return true
+  }, ['v'])
+
+  graph.add('outer', graph.subgraph)
+    .builds('!add-v').using({v: 1})
+    .builds('inner')
+
+  graph.add('inner', graph.subgraph)
+    .builds('!add-v').using({v: 2})
+    .builds('innerinner')
+
+  graph.add('innerinner', graph.subgraph)
+    .builds('!add-v').using({v: 3})
+    .builds({v4: 'add-v'}).using({v: 4})
+
+  return graph.newBuilder('descendant')
+    .builds('outer')
+    .run()
+    .then(function (data) {
+      test.equal(true, data['outer'])
+      test.equal('1234', log)
+    })
+})
+
+builder.add(function testImportantDescendant2(test) {
+  var log = ''
+  graph.add('add-v', function (v) {
+    log += v
+    return true
+  }, ['v'])
+
+  graph.add('outer', graph.subgraph)
+    .builds('!inner')
+    .builds('add-v').using({v: 4})
+
+  graph.add('inner', graph.subgraph)
+    .builds('!innerinner')
+    .builds('add-v').using({v: 3})
+
+  graph.add('innerinner', graph.subgraph)
+    .builds({'!v1': 'add-v'}).using({v: 1})
+    .builds('add-v').using({v: 2})
+
+  return graph.newBuilder('descendant')
+    .builds('outer')
+    .run()
+    .then(function (data) {
+      test.equal(true, data['outer'])
+      test.equal('1234', log)
+    })
+})
+
+builder.add(function testImportantCycle(test) {
+  // Try to create a cycle between one and two
+  var log = ''
+  graph.add('one', function () {
+    log += 1
+    return 1
+  })
+  graph.add('two', function () {
+    log += 2
+    return 2
+  })
+
+  graph.add('important-one', function () { return 1 })
+    .builds('one')
+
+  graph.add('important-two', function () { return 2 })
+    .builds('two')
+
+  graph.add('inner-one', graph.subgraph)
+    .builds('!important-two')
+    .builds('one')
+
+  graph.add('inner-two', graph.subgraph)
+    .builds('!important-one')
+    .builds('two')
+
+  graph.add('outer', function (a, b) {
+      return a + b
+    })
+    .builds('inner-one')
+    .builds('inner-two')
+
+  return graph.newBuilder('descendant')
+    .builds('outer')
+    .run()
+    .then(function (data) {
+      test.equal(3, data['outer'])
+      test.equal('21', log)
+    })
+})
